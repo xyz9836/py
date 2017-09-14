a43bd9f238bd3cbae13c6ad7778e0cf328163612
wsw <xyz9836@126.com>
2017-09-14 22:51:27 +0800

    1
    1

--- restart.py
+++ restart.py
@@ -1,13 +1,15 @@
 import urllib
-import urllib2
-test_data = {'Authorization':'ns7i54n3iuln78dtot936b4sjknjaoxr93po8hsw'}
-test_data_urlencode = urllib.urlencode(test_data)
+import httplib
+#test_data = {'ServiceCode':'aaaa','b':'bbbbb'}
+#test_data_urlencode = urllib.urlencode(test_data)
 requrl = "https://openapi.daocloud.io/v1/apps/922fc621-dec5-45ce-beb1-5279d5b278d6/actions/restart"
-req = urllib2.Request(url = requrl,data =test_data_urlencode)
-print req
-res_data = urllib2.urlopen(req)
-res = res_data.read()
+headerdata = {"Authorization":"ns7i54n3iuln78dtot936b4sjknjaoxr93po8hsw"}
+conn = httplib.HTTPConnection("https://openapi.daocloud.io")
+conn.request(method="POST",url=requrl,headers = headerdata)
+response = conn.getresponse()
+res= response.read()
 print res
+
 
 
 #  File "send2.py", line 3
