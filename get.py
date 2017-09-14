a18fe4a8862962aeac30335b1186432930d5a8fd
wsw <xyz9836@126.com>
2017-09-14 22:05:54 +0800

    1
    1

--- get.py
+++ get.py
@@ -1,7 +1,13 @@
 import httplib
-url = "http://sc.ftqq.com/SCU10361T0a2416cf6a6ca09da852bf223a588c2f59776131cef7d.send?text=PYTHON&desp=主人，这是发送信息测试~~"
+url = "http://sc.ftqq.com/SCU10361T0a2416cf6a6ca09da852bf223a588c2f59776131cef7d.send?text=PYTHON&desp=getting test1~~"
 conn = httplib.HTTPConnection("http://sc.ftqq.com")
 conn.request(method="GET",url=url)
 response = conn.getresponse()
 res= response.read()
 print res
+
+
+
+
+  #File "get.py", line 2
+#SyntaxError: Non-ASCII character '\xe4' in file get.py on line 2, but no encoding declared; see http://www.python.org/peps/pep-0263.html for details
--- send2.py
+++ send2.py
@@ -1,6 +1,6 @@
 import urllib
 import urllib2
-test_data = {'text':'PYTHON','desp':'主人，这是发送信息测试~~'}
+test_data = {'text':'PYTHON','desp':'sending tese2~~'}
 test_data_urlencode = urllib.urlencode(test_data)
 requrl = "https://sc.ftqq.com/SCU10361T0a2416cf6a6ca09da852bf223a588c2f59776131cef7d.send"
 req = urllib2.Request(url = requrl,data =test_data_urlencode)
@@ -8,3 +8,7 @@ print req
 res_data = urllib2.urlopen(req)
 res = res_data.read()
 print res
+
+
+#  File "send2.py", line 3
+#SyntaxError: Non-ASCII character '\xe4' in file send2.py on line 3, but no encoding declared; see http://www.python.org/peps/pep-0263.html for details
